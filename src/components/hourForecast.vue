d940b8c45ab9c7f988c753cd396ec168b3c4df2c
Alex Olexyuk <alex.olexyuk@namecheap.com>
2017-11-19 19:51:53 +0200

    add daily adn hourly forecast

---
+++ src/components/hourForecast.vue
@@ -0,0 +1,36 @@
+<template>
+  <li class="hour-forecast">
+      <i :class="classMap"></i>
+      <p class="time">
+        {{time}}
+      </p>
+      <p class="temp">{{hours.main.temp}} &#x2103;</p>
+      <p class="pressure">{{hours.main.pressure}} hpa</p>  
+      <p class="humidity">{{hours.main.humidity}} %</p>
+      <p>{{hours.wind.speed}} m/s</p>
+  </li>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  name: "hourForecast",
+  props: ["hours"],
+  computed: {
+    time() {
+      return moment(this.hours.dt_txt).format("LT");
+    },
+    classMap() {
+      return `owi owi-2x owi-${this.hours.weather[0].icon}`;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.hour-forecast {
+  padding: 0 8px;
+}
+</style>
+
